45d1d38902dffc59984b102c5c8db9ebc7a82b7f
Sam Sze <mingsum.sam@gmail.com>
2024-01-24 18:02:17 +0800

    add api-key interceptor

--- spring-boot-mvc/src/main/java/com/example/AppConfig.java
+++ spring-boot-mvc/src/main/java/com/example/AppConfig.java
@@ -1,13 +1,20 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.filters.LoggingFilter;
+import com.example.interceptors.ApiKeyInterceptor;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiKeyInterceptor apiKeyInterceptor;
 
     @Bean
     public FilterRegistrationBean<LoggingFilter> loggingFilter() {
@@ -15,5 +22,10 @@ public class AppConfig {
         registrationBean.setFilter(new LoggingFilter());
         registrationBean.addUrlPatterns("/*");
         return registrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiKeyInterceptor).excludePathPatterns("/manage/**");
     }
 }
--- spring-boot-mvc/src/main/java/com/example/ExampleSpringBootApplication.java
+++ spring-boot-mvc/src/main/java/com/example/ExampleSpringBootApplication.java
@@ -3,10 +3,14 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ExampleSpringBootApplication {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("API_KEY", dotenv.get("API_KEY"));
     SpringApplication.run(ExampleSpringBootApplication.class, args);
   }
 
